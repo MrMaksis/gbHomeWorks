71a3a76c618a64d0616ce2a4f073d6402171cc5a
mr_maksis <max.morink@yandex.ru>
2022-12-17 14:29:38 +0300

    Completed 3 task

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,9 @@
-﻿Console.WriteLine("Task_3");
+﻿Console.WriteLine("Task_3 \n");
 
+Console.Write("Введите число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber % 2 == 0)
+    Console.WriteLine($"Число {firstNumber} чётное.");
+else
+    Console.WriteLine($"Число {firstNumber} не чётное.");
