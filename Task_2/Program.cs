19f535d645c02ba2f67a72354753efb8f1a66014
mr_maksis <max.morink@yandex.ru>
2022-12-17 14:22:36 +0300

    Completed 2 task

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Task_2");
+﻿Console.WriteLine("Task_2 \n");
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {Math.Max(Math.Max(firstNumber, secondNumber), thirdNumber)} самое большое."); //Тут я решил воспользоваться готовой функцией нахождения max числа
+
+
