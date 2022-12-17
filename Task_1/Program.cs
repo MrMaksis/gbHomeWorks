c535c9114ccf927179c076338da719b1eb4af569
mr_maksis <max.morink@yandex.ru>
2022-12-17 13:52:34 +0300

    Completed 1 task

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Task_1");
+﻿Console.WriteLine("Task_1 \n");
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+    Console.WriteLine($"Число {firstNumber} больше.");
+if (secondNumber > firstNumber)
+    Console.WriteLine($"Число {secondNumber} больше.");
+if (firstNumber < secondNumber)
+    Console.WriteLine($"Число {firstNumber} меньше.");
+if (secondNumber < firstNumber)
+    Console.WriteLine($"Число {secondNumber} меньше.");
+    
