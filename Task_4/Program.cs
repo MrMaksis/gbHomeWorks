d183b0ed6cebacb172b276d45168c55bf92081db
mr_maksis <max.morink@yandex.ru>
2022-12-18 12:18:10 +0300

    Completed 4 task

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Task_4");
+﻿Console.WriteLine("Task_4 \n");
+
+Console.Write("Введите число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while(count <= firstNumber){
+    if (count % 2 == 0){
+        Console.WriteLine(count);
+    }
+    count++;
+} 
